fa74f0e9f2c7dda8cd875b38cfe6ca44e5c3b162
Madhumida <smadhumida@gmail.com>
2022-11-12 22:37:30 -0800

    Checks result

--- src/pages/AddressScreen.js
+++ src/pages/AddressScreen.js
@@ -17,6 +17,7 @@ export default function AddressScreen() {
         var result = {
             quadrant: 'northeast'
         }
+        console.log(result)
         setAddressResult(result);
     }
     return (
